395ed25daf45e04f83c209f551f28f8fa84e0924
H1trec <51777668+H1trec@users.noreply.github.com>
2025-04-03 18:33:11 +0300

    Delete H2 directory

--- H2/main.py
+++
@@ -1,9 +0,0 @@
-from controller import PhonebookController
-
-def main():
-    filename = 'phones.json'
-    controller = PhonebookController(filename)
-    controller.run()
-
-if __name__ == '__main__':
-    main()
